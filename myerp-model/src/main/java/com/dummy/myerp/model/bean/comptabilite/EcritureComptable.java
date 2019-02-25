2ae75c9546f69c5f5171d0ad227fe3afa8a40d9f
lopotono <30410435+lopotono@users.noreply.github.com>
2019-02-25 15:45:39 +0100

    Correction de l'erreur dans regexp

--- myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
+++ myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
@@ -24,7 +24,7 @@ public class EcritureComptable {
     /** Journal comptable */
     @NotNull private JournalComptable journal;
     /** The Reference. */
-    @Pattern(regexp = "\\d{1,5}-\\d{4}/\\d{5}")
+    @Pattern(regexp = "\\w{2}-\\d{4}/\\d{5}") //erreur corrigée remplacement d par w (2 lettres)
     private String reference;
     /** The Date. */
     @NotNull private Date date;
@@ -80,7 +80,7 @@ public class EcritureComptable {
      *
      * @return {@link BigDecimal}, {@link BigDecimal#ZERO} si aucun montant au débit
      */
-    // TODO à tester : OK
+    // TODO à tester
     public BigDecimal getTotalDebit() {
         BigDecimal vRetour = BigDecimal.ZERO;
         for (LigneEcritureComptable vLigneEcritureComptable : listLigneEcriture) {
