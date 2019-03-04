1117a93dd7ce746b525f86efee0ff2a5362abc17
lopotono <30410435+lopotono@users.noreply.github.com>
2019-03-04 15:42:12 +0100

    Update ComptabiliteManager.java

--- myerp-business/src/main/java/com/dummy/myerp/business/contrat/manager/ComptabiliteManager.java
+++ myerp-business/src/main/java/com/dummy/myerp/business/contrat/manager/ComptabiliteManager.java
@@ -6,6 +6,7 @@ import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.technical.exception.FunctionalException;
+import com.dummy.myerp.technical.exception.NotFoundException;
 
 
 /**
@@ -49,8 +50,11 @@ public interface ComptabiliteManager {
      *
      * <p><strong>Attention :</strong> l'écriture n'est pas enregistrée en persistance</p>
      * @param pEcritureComptable L'écriture comptable concernée
+     * @throws FunctionalException 
+     * @throws NotFoundException 
+     * @throws NumberFormatException 
      */
-    void addReference(EcritureComptable pEcritureComptable);
+    void addReference(EcritureComptable pEcritureComptable) throws FunctionalException, NumberFormatException, NotFoundException;
 
     /**
      * Vérifie que l'Ecriture comptable respecte les règles de gestion.
@@ -82,4 +86,7 @@ public interface ComptabiliteManager {
      * @param pId l'id de l'écriture
      */
     void deleteEcritureComptable(Integer pId);
+    
+    EcritureComptable getEcritureComptable(Integer pId) throws NotFoundException;
+  
 }
