22229a5c547397f070326966cb09e6ad3a13a988
lopotono <30410435+lopotono@users.noreply.github.com>
2019-03-15 09:30:44 +0100

    Update JournalComptableTest.java

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -7,18 +7,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class JournalComptableTest {
+	
+	private static List<JournalComptable> vList;
 
 	@Test
 	public void testToString() {
-		
 		JournalComptable vJournal = new JournalComptable();
-		
+
 		String vCode = "AC";
 		vJournal.setCode(vCode);
 		String vLibelle = "Achat";
 		vJournal.setLibelle(vLibelle);
-		String toStringResult = "JournalComptable{code='"+vCode+"', libelle='"+vLibelle+"'}";
-		
+		String toStringResult = "JournalComptable{code='" + vCode + "', libelle='" + vLibelle + "'}";
+
 		Assert.assertEquals(vCode, vJournal.getCode());
 		Assert.assertEquals(vLibelle, vJournal.getLibelle());
 		Assert.assertEquals(vJournal.toString(), toStringResult);
@@ -27,16 +28,15 @@ public class JournalComptableTest {
 
 	@Test
 	public void testGetByCode() {
-				
-		JournalComptable vJournal = new JournalComptable();
-		
+		/*JournalComptable vJournal = new JournalComptable();
 		List<String> pList = new ArrayList<String>();
 		String vCode = "AC";
 		vJournal.setCode(vCode);
 		pList.add(vCode);
-			
-		Assert.assertEquals(vCode, vJournal.getCode());
-
+		Assert.assertEquals(vCode, vJournal.getCode());*/
+		vList = new ArrayList<>();
+		vList.add(new JournalComptable("AC", "Achat"));
+		Assert.assertEquals(JournalComptable.getByCode(vList, "AC"), vList.get(0));
 	}
-	
+
 }
